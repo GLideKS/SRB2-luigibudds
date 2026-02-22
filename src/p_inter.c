27ab99658a2721c4fecb5016144e60673a77dd8f
MIDIMan <miditheman2.0@gmail.com>
2026-02-22 16:50:04 -0500

    Add nil inflictor checks in p_inter.c to prevent SIGSEGVs

--- src/p_inter.c
+++ src/p_inter.c
@@ -3227,7 +3227,7 @@ static boolean P_TagDamage(mobj_t *target, mobj_t *inflictor, mobj_t *source, IN
 	if ((player->pflags & PF_TAGIT && source && source->player && !(((cv_friendlyfire.value || (gametyperules & GTR_FRIENDLYFIRE)) || ((damagetype & DMG_CANHURTSELF) && source->player == player)) &&
 		source->player->pflags & PF_TAGIT)))
 	{
-		if (inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
+		if (inflictor && inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
 		{
 			if (player->revitem != MT_LHRT && player->spinitem != MT_LHRT && player->thokitem != MT_LHRT) // Healers do not get to heal other healers.
 			{
@@ -3243,22 +3243,25 @@ static boolean P_TagDamage(mobj_t *target, mobj_t *inflictor, mobj_t *source, IN
 	if (source && source->player && !((cv_friendlyfire.value || (gametyperules & GTR_FRIENDLYFIRE)) || ((damagetype & DMG_CANHURTSELF) && source->player == player)) && 
 		(player->pflags & PF_TAGIT) == (source->player->pflags & PF_TAGIT))
 	{
-		if (inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
+		if (inflictor)
 		{
-			if (player->revitem != MT_LHRT && player->spinitem != MT_LHRT && player->thokitem != MT_LHRT) // Healers do not get to heal other healers.
+			if (inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
 			{
-				P_SwitchShield(player, SH_PINK);
-				S_StartSoundFromMobj(target, mobjinfo[MT_PITY_ICON].seesound);
+				if (player->revitem != MT_LHRT && player->spinitem != MT_LHRT && player->thokitem != MT_LHRT) // Healers do not get to heal other healers.
+				{
+					P_SwitchShield(player, SH_PINK);
+					S_StartSoundFromMobj(target, mobjinfo[MT_PITY_ICON].seesound);
+				}
 			}
+			else if (!(inflictor->flags & MF_FIRE))
+				P_GivePlayerRings(player, 1);
+			if (inflictor->flags2 & MF2_BOUNCERING)
+				inflictor->fuse = 0; // bounce ring disappears at -1 not 0
 		}
-		else if (!(inflictor->flags & MF_FIRE))
-			P_GivePlayerRings(player, 1);
-		if (inflictor->flags2 & MF2_BOUNCERING)
-			inflictor->fuse = 0; // bounce ring disappears at -1 not 0
 		return false;
 	}
 
-	if (inflictor->type == MT_LHRT)
+	if (inflictor && inflictor->type == MT_LHRT)
 		return false;
 
 	// The tag occurs so long as you aren't shooting another tagger with friendlyfire on.
@@ -3328,7 +3331,7 @@ static boolean P_PlayerHitsPlayer(mobj_t *target, mobj_t *inflictor, mobj_t *sou
 		// In COOP/RACE, you can't hurt other players unless cv_friendlyfire is on
 		if (!(cv_friendlyfire.value || (gametyperules & GTR_FRIENDLYFIRE)) && (gametyperules & GTR_FRIENDLY))
 		{
-			if ((gametyperules & GTR_FRIENDLY) && inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK)) // co-op only
+			if ((gametyperules & GTR_FRIENDLY) && inflictor && inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK)) // co-op only
 			{
 				if (player->revitem != MT_LHRT && player->spinitem != MT_LHRT && player->thokitem != MT_LHRT) // Healers do not get to heal other healers.
 				{
@@ -3351,24 +3354,27 @@ static boolean P_PlayerHitsPlayer(mobj_t *target, mobj_t *inflictor, mobj_t *sou
 		// unless cv_friendlyfire is on.
 		if (!(cv_friendlyfire.value || (gametyperules & GTR_FRIENDLYFIRE)) && target->player->ctfteam == source->player->ctfteam)
 		{
-			if (inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
+			if (inflictor)
 			{
-				if (player->revitem != MT_LHRT && player->spinitem != MT_LHRT && player->thokitem != MT_LHRT) // Healers do not get to heal other healers.
+				if (inflictor && inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
 				{
-					P_SwitchShield(player, SH_PINK);
-					S_StartSoundFromMobj(target, mobjinfo[MT_PITY_ICON].seesound);
+					if (player->revitem != MT_LHRT && player->spinitem != MT_LHRT && player->thokitem != MT_LHRT) // Healers do not get to heal other healers.
+					{
+						P_SwitchShield(player, SH_PINK);
+						S_StartSoundFromMobj(target, mobjinfo[MT_PITY_ICON].seesound);
+					}
 				}
+				else if (!(inflictor->flags & MF_FIRE))
+					P_GivePlayerRings(target->player, 1);
+				if (inflictor->flags2 & MF2_BOUNCERING)
+					inflictor->fuse = 0; // bounce ring disappears at -1 not 0
 			}
-			else if (!(inflictor->flags & MF_FIRE))
-				P_GivePlayerRings(target->player, 1);
-			if (inflictor->flags2 & MF2_BOUNCERING)
-				inflictor->fuse = 0; // bounce ring disappears at -1 not 0
 
 			return false;
 		}
 	}
 
-	if (inflictor->type == MT_LHRT)
+	if (inflictor && inflictor->type == MT_LHRT)
 		return false;
 
 	// Add pity.
@@ -3615,7 +3621,7 @@ void P_SpecialStageDamage(player_t *player, mobj_t *inflictor, mobj_t *source)
 
 	if (!cv_friendlyfire.value && source && source->player)
 	{
-		if (inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
+		if (inflictor && inflictor->type == MT_LHRT && !(player->powers[pw_shield] & SH_NOSTACK))
 		{
 			if (player->revitem != MT_LHRT && player->spinitem != MT_LHRT && player->thokitem != MT_LHRT) // Healers do not get to heal other healers.
 			{
