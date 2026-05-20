f88ed2f528cbfb3d86eb0d84e6fd1a80dc162547
romoney5 <whygetmugged@gmail.com>
2026-05-20 15:48:36 -0400

    Merge branch 'textinput-hook' into only-luamenu

--- src/lua_hook.h
+++ src/lua_hook.h
@@ -77,6 +77,7 @@ automatically.
 	X (CameraThinker),/* P_MoveChaseCamera */\
 	X (KeyDown),\
 	X (KeyUp),\
+	X (TextInput),\
 
 #define STRING_HOOK_LIST(X) \
 	X (BotAI),/* B_BuildTailsTiccmd by skin name */\
@@ -137,6 +138,7 @@ void LUA_HookBool(boolean value, int hook);
 int  LUA_HookPlayer(player_t *, int hook);
 int  LUA_HookTiccmd(player_t *, ticcmd_t *, int hook);
 int  LUA_HookKey(event_t *event, int hook); // Hooks for key events
+int  LUA_HookText(event_t *event, int hook); // Hooks for text events
 
 void LUA_HookPreThinkFrame(void);
 void LUA_HookThinkFrame(void);
