a7bd67c3a56ad62dbac6bc217022b789e315d7d5
romoney5 <whygetmugged@gmail.com>
2026-07-02 19:04:02 -0400

    Add CameraThinker hook (Ported from SRB2 Banpyura)

--- src/lua_hook.h
+++ src/lua_hook.h
@@ -80,6 +80,7 @@ automatically.
 	X (PlayerHeight),/* override player height */\
 	X (PlayerCanEnterSpinGaps),\
 	X (AddonLoaded),\
+	X (CameraThinker),/* P_MoveChaseCamera */\
 	X (KeyDown),\
 	X (KeyUp),\
 	X (TextInput),\
@@ -167,6 +168,7 @@ int  LUA_HookPlayerMsg(int source, int target, int flags, char *msg);
 int  LUA_HookHurtMsg(player_t *, mobj_t *inflictor, mobj_t *source, UINT8 damagetype);
 int  LUA_HookMapThingSpawn(mobj_t *, mapthing_t *);
 int  LUA_HookFollowMobj(player_t *, mobj_t *);
+int  LUA_HookCameraThinker(player_t *, camera_t *);
 int  LUA_HookPlayerCanDamage(player_t *, mobj_t *);
 void LUA_HookPlayerQuit(player_t *, kickreason_t);
 int  LUA_HookNameChange(player_t *plr, const char *name);
