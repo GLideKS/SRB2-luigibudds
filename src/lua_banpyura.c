982e6c119d2576e230337e51aa07d10b00e5b349
romoney5 <whygetmugged@gmail.com>
2026-08-04 12:30:41 -0400

    Move Lua cvar stuff into the Banpyura library

--- src/lua_banpyura.c
+++ src/lua_banpyura.c
@@ -9,6 +9,7 @@
 /// \file  lua_banpyura.c
 /// \brief Custom Lua functions for SRB2 Banpyura
 
+#include "p_local.h"
 #include "lua_script.h"
 #include "lua_libs.h"
 
@@ -36,9 +37,76 @@ static int lib_SpriteShadow_SetAngle(lua_State *L)
 	return 0;
 }
 
+// Banpyura.CV_GetPossibleValue(consvar_t cvar, int num)
+// this could be exposed as userdata but i want to keep modding features clean
+// this SHOULD return a table
+static int lib_CV_GetPossibleValue(lua_State *L)
+{
+	consvar_t *cvar = *((consvar_t **)luaL_checkudata(L, 1, META_CVAR));
+	INT32 num = luaL_checkinteger(L, 2);
+	if (!cvar)
+		return LUA_ErrInvalid(L, "consvar_t");
+
+	// make sure it's valid
+	for (INT32 i = 0; i < num; i++)
+		if (cvar->PossibleValue[i].strvalue == NULL)
+			return 0;
+
+	CV_PossibleValue_t pv = cvar->PossibleValue[num];
+
+	lua_pushinteger(L, pv.value);
+	lua_pushstring(L, pv.strvalue);
+
+	return 2;
+}
+
+// Banpyura.CV_ForceAddValue(consvar_t cvar, int value)
+// this is needed to set cvars by force
+static int lib_CV_ForceAddValue(lua_State *L)
+{
+	consvar_t *cvar = *(consvar_t **)luaL_checkudata(L, 1, META_CVAR);
+
+	//if (!(cvar->flags & CV_ALLOWLUA))
+	//	return luaL_error(L, "Variable %s cannot be set from Lua.", cvar->name);
+
+	CV_AddValue(cvar, (INT32)luaL_checknumber(L, 2));
+
+	return 0;
+}
+
+// Banpyura.CV_ForceSet(consvar_t cvar, int value)
+// this is needed to set cvars by force
+static int lib_CV_ForceSet(lua_State *L)
+{
+	consvar_t *cvar = *(consvar_t **)luaL_checkudata(L, 1, META_CVAR);
+
+	//if (!(cvar->flags & CV_ALLOWLUA))
+	//	return luaL_error(L, "Variable '%s' cannot be set from Lua.", cvar->name);
+
+	switch (lua_type(L, 2))
+	{
+	case LUA_TSTRING:
+		CV_Set(cvar, lua_tostring(L, 2));
+		break;
+	case LUA_TNUMBER:
+		CV_SetValue(cvar, (INT32)lua_tonumber(L, 2));
+		break;
+	default:
+		return luaL_typerror(L, 1, "string or number");
+	}
+
+	return 0;
+}
+// actually, why is it called luaL_typerror and not luaL_typeerror?
+
 
 static luaL_Reg Banpyura[] = {
 	{"SpriteShadow_SetAngle", lib_SpriteShadow_SetAngle},
+
+	{"CV_GetPossibleValue", lib_CV_GetPossibleValue},
+	{"CV_ForceAddValue", lib_CV_ForceAddValue},
+	{"CV_ForceSet", lib_CV_ForceSet},
+
 	{NULL, NULL}
 };
 
--- src/lua_consolelib.c
+++ src/lua_consolelib.c
@@ -577,7 +577,7 @@ static int CVarSetFunction
 ){
 	consvar_t *cvar = *(consvar_t **)luaL_checkudata(L, 1, META_CVAR);
 
-	if (!(cvar->flags & CV_ALLOWLUA) && !lua_optboolean(L, 3))
+	if (!(cvar->flags & CV_ALLOWLUA))
 		return luaL_error(L, "Variable '%s' cannot be set from Lua.", cvar->name);
 
 	switch (lua_type(L, 2))
@@ -609,44 +609,10 @@ static int lib_cvAddValue(lua_State *L)
 {
 	consvar_t *cvar = *(consvar_t **)luaL_checkudata(L, 1, META_CVAR);
 
-	if (!(cvar->flags & CV_ALLOWLUA) && !lua_optboolean(L, 3))
+	if (!(cvar->flags & CV_ALLOWLUA))
 		return luaL_error(L, "Variable %s cannot be set from Lua.", cvar->name);
 
 	CV_AddValue(cvar, (INT32)luaL_checknumber(L, 2));
-
-	return 0;
-}
-
-// romoney5 TODO: expose as userdata
-static int lib_cvGetPossibleValue(lua_State *L)
-{
-	consvar_t *cvar = *((consvar_t **)luaL_checkudata(L, 1, META_CVAR));
-	INT32 num = luaL_checkinteger(L, 2);
-	if (!cvar)
-		return LUA_ErrInvalid(L, "consvar_t");
-
-	// make sure it's valid
-	for (INT32 i = 0; i < num; i++)
-		if (cvar->PossibleValue[i].strvalue == NULL)
-			return 0;
-
-	CV_PossibleValue_t pv = cvar->PossibleValue[num];
-
-	lua_pushinteger(L, pv.value);
-	lua_pushstring(L, pv.strvalue);
-
-	return 2;
-}
-
-// romoney5 TODO: expose as userdata
-static int lib_cvSetFlags(lua_State *L)
-{
-	consvar_t *cvar = *((consvar_t **)luaL_checkudata(L, 1, META_CVAR));
-	INT32 flags = luaL_checkinteger(L, 2);
-	if (!cvar)
-		return LUA_ErrInvalid(L, "consvar_t");
-
-	cvar->flags = flags;
 
 	return 0;
 }
@@ -695,8 +661,6 @@ static luaL_Reg lib[] = {
 	{"CV_StealthSet", lib_cvStealthSet},
 	{"CV_AddValue", lib_cvAddValue},
 	{"CONS_Printf", lib_consPrintf},
-	{"CV_GetPossibleValue", lib_cvGetPossibleValue},
-	{"CV_SetFlags", lib_cvSetFlags},
 	{NULL, NULL}
 };
 
